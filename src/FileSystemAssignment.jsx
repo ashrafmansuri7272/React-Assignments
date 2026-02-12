590f0c2a9c7e3eb85bdd66aa8ab7955b58dedb39
ashrafmansuri7272 <60643228+ashrafmansuri7272@users.noreply.github.com>
2026-02-12 10:03:54 +0000

    file system parent file correction

--- src/FileSystemAssignment.jsx
+++ src/FileSystemAssignment.jsx
@@ -1,18 +1,13 @@
 import React, { useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import { mockFileTree } from "./constants";
+import { FileNode } from "./FileNode";
 
-export const FileNode = ({ node }) => {
-    console.log(node);
-    const [isNodeOpen, setIsNodeOpen] = useState(false);
-    return (
-        <div style={{cursor: 'pointer'}}>
-            <span onClick={() => setIsNodeOpen(prev => !prev)}>{`${node.children ? (isNodeOpen ? '[-]' : '[+]') : '[ ]'} ${node.name}`} </span>
-            <ul >
-                {isNodeOpen && node.children?.map((child) => (
-                    <li key={child.id}>
-                        <FileNode node={child} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+export const FileSystemAssignment = () => {
+  return (
+  <div>
+    <FileNode node={mockFileTree} />
+    <Link to="/">Back to Assignments</Link>
+  </div>
+  );
 }
